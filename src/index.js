0e690043499c55c1ea7e87df4fb1723ac9c1521a
Ryan Chen <ryan917rc@gmail.com>
2023-10-14 17:11:06 -0400

    added remove button for books

--- src/index.js
+++ src/index.js
@@ -20,21 +20,28 @@ function renderBooks() {
     while (objectDiv.firstChild) {
         objectDiv.removeChild(objectDiv.firstChild);
     }
+    
     for (let i = 0; i < myLibrary.length; i++) {
         console.log(i)
         let newDiv = document.createElement("div");
         let titlevar = document.createElement("h1");
         let authorvar = document.createElement("h1");
         let pagesvar = document.createElement("h1");
+        let removeBut = document.createElement("button");
         let myBook = myLibrary[i]
 
         titlevar.textContent = "Title: " + myBook.title;
         authorvar.textContent = "Author: " + myBook.author;
         pagesvar.textContent = "# of pages: " + myBook.pages;
+        removeBut.textContent = "remove"
+        removeBut.onclick = function() {
+            myLibrary.splice(i, 1)
+            renderBooks()
+        }
         newDiv.appendChild(titlevar)
         newDiv.appendChild(authorvar)
         newDiv.appendChild(pagesvar)
-
+        newDiv.appendChild(removeBut)
         newDiv.className = "object"
         objectDiv.appendChild(newDiv)
     
